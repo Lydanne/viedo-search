0d27a7f0a82ddcf36763b018a203ae6e63558c9f
WumaCoder <1829913225@qq.com>
2020-07-06 22:39:21 +0800

    APP:修改BUG

--- src/viedo-search/common/apis/gitee.js
+++ src/viedo-search/common/apis/gitee.js
@@ -1,7 +1,7 @@
 import axios from "@/common/tools/http.js"
 import { looseJsonParse } from "@/common/tools"
 import { Base64 } from 'js-base64'
-import { source } from '@/config.js' 
+import config from '@/config.js' 
 
 /**
  * 获取源信息
@@ -13,7 +13,7 @@ import { source } from '@/config.js'
  *  }]
  */
 export async function getSources(){
-  const res = await axios.get(source);
+  const res = await axios.get(config.source);
   return res.data.tree.filter(item => item.path.search(/^sources\//gm)!==-1);
 }
 
